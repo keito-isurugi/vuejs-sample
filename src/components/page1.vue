06715777630c250af2eab8969659c0dd848a11f7
KeitoIsurugi <ja224kg@gmail.com>
2021-07-09 08:12:13 +0900

    vuejs sample

---
+++ src/components/page1.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>
+      sssss
+    </p>
+    <p>
+      ここはページ１です。
+    </p>
+    <router-link to="/page2">Go to page2</router-link>
+    <p>
+      sssss
+    </p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
